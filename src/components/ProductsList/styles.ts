1b83a4e122e3f304ac36f17186f6b02d6a10d027
Rafael da Silva Muniz <rafaelmunizrsm@hotmail.com>
2024-12-27 21:24:34 -0300

    Final

---
+++ src/components/ProductsList/styles.ts
@@ -0,0 +1,41 @@
+import styled from 'styled-components'
+
+import { Props } from '.'
+import { breakpoints, cores } from '../../styles'
+import { Card } from '../Product/styles'
+
+export const Container = styled.section<Omit<Props, 'title' | 'massas'>>`
+  background-color: ${(props) =>
+    props.background === 'black' ? cores.fundo : cores.salmao};
+
+  ${Card} {
+    background-color: ${(props) =>
+      props.background === 'white' ? cores.fundo : cores.salmao};
+  }
+`
+
+export const List = styled.ul`
+  display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  column-gap: 20px;
+  margin-top: 80px;
+
+  @media (max-width: ${breakpoints.desktop} or min-width: ${breakpoints.celular}) {
+    margin-left: 0;
+    display: block;
+    justify-content: center;
+  }
+`
+
+/*display: grid;
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  column-gap: 24px;
+  margin-top: 40px;
+  padding: 24px;
+  padding: 32px 0;*/
+
+export const Title = styled.h2`
+  font-size: 18px;
+  font-weight: bold;
+  color: ${cores.salmao};
+`
